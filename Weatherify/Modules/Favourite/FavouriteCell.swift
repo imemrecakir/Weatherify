f9e1b2dc5012ba10ea1a4adad24fac096c935b51
Emre Çakır <imemrecakir@gmail.com>
2024-03-02 14:56:00 +0300

    FavouriteViewController updated

---
+++ Weatherify/Modules/Favourite/FavouriteCell.swift
@@ -0,0 +1,112 @@
+//
+//  FavouriteCell.swift
+//  Weatherify
+//
+//  Created by Emre Çakır on 2.03.2024.
+//
+
+import UIKit
+
+protocol FavouriteCellDelegate: AnyObject {
+    func didRemoveTapped(index: Int)
+}
+
+final class FavouriteCell: UICollectionViewCell {
+    
+    static let reuseIdentifier = "FavouriteCell"
+    
+    weak var delegate: FavouriteCellDelegate?
+    
+    private var index = 0
+    
+    private lazy var removeButton: UIButton = {
+        var configuration = UIButton.Configuration.tinted()
+        configuration.imagePadding = 8
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .clear
+        button.configuration = configuration
+        button.configuration?.baseBackgroundColor = .clear
+        button.imageView?.backgroundColor = .clear
+        button.setImage(UIImage(systemName: "bookmark.slash.fill"), for: .normal)
+        button.addTarget(self, action: #selector(removeButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var contentStackView: UIStackView = {
+       let stackView = UIStackView(arrangedSubviews: [cityNameLabel, countryNameLabel, temperatureLabel])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        stackView.alignment = .center
+        return stackView
+    }()
+    
+    private lazy var cityNameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .label
+        label.font = .monospacedSystemFont(ofSize: 18, weight: .medium)
+        label.setContentHuggingPriority(.required, for: .vertical)
+        return label
+    }()
+    
+    private lazy var countryNameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .secondaryLabel
+        label.font = .monospacedSystemFont(ofSize: 12, weight: .regular)
+        label.setContentHuggingPriority(.required, for: .vertical)
+        return label
+    }()
+    
+    private lazy var temperatureLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.font = .monospacedSystemFont(ofSize: 12, weight: .regular)
+        label.setContentHuggingPriority(.required, for: .vertical)
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+        setupConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        backgroundColor = .systemFill
+        clipsToBounds = true
+        layer.cornerRadius = 12
+        
+        contentView.addSubview(removeButton)
+        contentView.addSubview(contentStackView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            removeButton.topAnchor.constraint(equalTo: contentView.topAnchor),
+            removeButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            removeButton.widthAnchor.constraint(equalToConstant: 40),
+            removeButton.heightAnchor.constraint(equalToConstant: 40),
+            
+            contentStackView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            contentStackView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+    }
+    
+    func configureCell(city: String, country: String, temperature: Double, index: Int) {
+        cityNameLabel.text = city
+        countryNameLabel.text = country
+        temperatureLabel.text = temperature.formattedTemperature()
+        self.index = index
+    }
+    
+    @objc private func removeButtonTapped() {
+        delegate?.didRemoveTapped(index: index)
+    }
+}
--- Weatherify/Modules/Favourite/FavouriteViewController.swift
+++ Weatherify/Modules/Favourite/FavouriteViewController.swift
@@ -7,10 +7,133 @@
 
 import UIKit
 
-final class FavouriteViewController: BaseViewController<FavouriteViewModel> {}
+final class FavouriteViewController: BaseViewController<FavouriteViewModel> {
+    
+    private var cities = ["Berlin", "Madrid", "İstanbul", "New York", "Londra"]
+    private var countries = ["Germany", "Spaint", "Turkey", "USA", "England"]
+    private var temperatures = [22, 23.4, 29.14, 18, 16]
+    
+    private lazy var removeAllFavouritesBarButtonItem: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "trash.slash.fill"), for: .normal)
+        button.isEnabled = !cities.isEmpty
+        button.addTarget(self, action: #selector(removeAllFavouritesBarButtonItemTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var favouritesCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .clear
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.contentInset = .init(top: 8, left: 16, bottom: 8, right: 16)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(FavouriteCell.self, forCellWithReuseIdentifier: FavouriteCell.reuseIdentifier)
+        return collectionView
+    }()
+    
+    private lazy var emptyMessageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "There is no favourite yet"
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.font = .monospacedSystemFont(ofSize: 18, weight: .medium)
+        label.center = favouritesCollectionView.center
+        label.sizeToFit()
+        return label
+    }()
+    
+    private func reloadData() {
+        removeAllFavouritesBarButtonItem.isEnabled = !cities.isEmpty
+        DispatchQueue.main.async { [weak self] in
+            self?.favouritesCollectionView.reloadData()
+        }
+    }
+    
+    @objc private func removeAllFavouritesBarButtonItemTapped() {
+        let alertController = UIAlertController(title: "Are you sure?", message: "You are deleting all favourites", preferredStyle: .alert)
+
+        alertController.addAction(
+            UIAlertAction(title: "Yes", style: .destructive) { [weak self] _ in
+                self?.cities.removeAll()
+                self?.countries.removeAll()
+                self?.temperatures.removeAll()
+                self?.reloadData()
+            }
+        )
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+}
 
 extension FavouriteViewController {
-    func setupUI() {}
+    func setupUI() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: removeAllFavouritesBarButtonItem)
+        view.addSubview(favouritesCollectionView)
+    }
     
-    func setupConstraints() {}
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            favouritesCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            favouritesCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            favouritesCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            favouritesCollectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
+
+extension FavouriteViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if cities.isEmpty {
+            collectionView.backgroundView = emptyMessageLabel
+        } else {
+            collectionView.backgroundView = nil
+        }
+        
+        return cities.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FavouriteCell.reuseIdentifier, for: indexPath) as? FavouriteCell {
+            cell.configureCell(city: cities[indexPath.item], country: cities[indexPath.item], temperature: temperatures[indexPath.item], index: indexPath.item)
+            cell.delegate = self
+            return cell
+        }
+        
+        return UICollectionViewCell(frame: .zero)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let collectionViewSize = collectionView.frame.size
+        let collectionViewHorizontalPadding = collectionView.contentInset.left + collectionView.contentInset.right + 32
+        let cellSize = (collectionViewSize.width - collectionViewHorizontalPadding) / 2
+        return .init(width: cellSize, height: cellSize)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 32
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        16
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let detailViewController = DetailViewController()
+        detailViewController.hidesBottomBarWhenPushed = true
+        navigationController?.pushViewController(detailViewController, animated: true)
+    }
+}
+
+extension FavouriteViewController: FavouriteCellDelegate {
+    func didRemoveTapped(index: Int) {
+        cities.remove(at: index)
+        countries.remove(at: index)
+        temperatures.remove(at: index)
+        reloadData()
+    }
 }
