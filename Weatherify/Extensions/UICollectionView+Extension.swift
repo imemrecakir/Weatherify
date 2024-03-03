7bd5b3342e3cb3bce365928e16e39457f09d27e0
Emre Çakır <imemrecakir@gmail.com>
2024-03-03 17:52:35 +0300

    EmptyView updated

--- Weatherify/Extensions/UICollectionView+Extension.swift
+++ Weatherify/Extensions/UICollectionView+Extension.swift
@@ -12,11 +12,11 @@ extension UICollectionView {
     func addEmptyView(message: String) {
         lazy var emptyMessageLabel: UILabel = {
             let label = UILabel()
-            label.text = "message"
+            label.text = message
             label.numberOfLines = 0
             label.textAlignment = .center
             label.font = .monospacedSystemFont(ofSize: 18, weight: .medium)
-            label.center = CGPoint(x: self.center.x, y: self.center.y - 100)
+            label.center = self.center
             label.sizeToFit()
             return label
         }()
